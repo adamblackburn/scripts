a637b16786b48dfc867417af2f16a993e2b8cf4f
Adam Blackburn <adam@adamblackburn.net>
2013-04-11 01:02:38 -0500

    added license clause

--- class/cs431/exact-schedulablility/schedcalc.py
+++ class/cs431/exact-schedulablility/schedcalc.py
@@ -1,12 +1,32 @@
 #!/usr/bin/env python
 
+# Copyright (c) 2013 Adam Blackburn
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+# of the Software, and to permit persons to whom the Software is furnished to do
+# so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import math
 
 # the execution times, in T1, T2, ..., TI order
-exec_times = [4, 6.1, 1]
+exec_times = [2, 10, 9]
 
 # the periods, in T1, T2, ..., TI order
-periods = [10, 14, 70]
+periods = [6, 24, 36]
 
 # the number of processes
 process_count = len(exec_times)
